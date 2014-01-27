9abc881cc9cebdff4cb10527ae8bf6da96c3a035
Scooterlicious <sfell2006@gmail.com>
2014-01-27 14:17:10 -0500

    Assignments contained in controller file

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,28 +1,35 @@
 class LunchesController < ApplicationController
+#Scooter
 #    lunches GET    /lunches(.:format)          lunches#index
 	def index
 	end
 	
+# Lena	
 #            POST   /lunches(.:format)          lunches#create
 	def create
 	end
 	
+#Lena
 #  new_lunch GET    /lunches/new(.:format)      lunches#new
 	def new
 	end
-	
+
+#Sarah 	
 # edit_lunch GET    /lunches/:id/edit(.:format) lunches#edit
 	def edit
 	end
 
+# Scooter	
 #      lunch GET    /lunches/:id(.:format)      lunches#show
 	def show
 	end
 
+#Sarah 
 #            PUT    /lunches/:id(.:format)      lunches#update
 	def update
 	end
 
+#Scooter #May need to use form helper
 #            DELETE /lunches/:id(.:format)      lunches#destroy
 	def destroy
 	end 
